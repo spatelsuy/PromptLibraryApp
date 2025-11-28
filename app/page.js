c847824f0bacbc1df4351d5dea24de4ef7ec24cf
Sunil Patel <126328736+spatelsuy@users.noreply.github.com>
2025-11-27 21:38:24 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,7 +5,9 @@ export default function HomePage() {
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 p-6">
       <div className="max-w-2xl text-center">
         <h1 className="text-4xl font-bold text-gray-600 mb-4">Welcome to Prompt Librar</h1>
-        <p className="text-lg text-gray-700 mb-6">your centralized hub for building, storing, and managing AI prompts. Whether you are experimenting, refining, or building production workflows, Prompt Library helps you keep everything organized and versioned, so you never lose track of your best ideas. Add new prompts, revisit older versions, and browse your growing prompt collection — all in one clean, simple interface.
+        <p className="text-lg text-gray-700 mb-6">your centralized hub for building, storing, and managing AI prompts. 
+        Whether you are experimenting, refining, or building production workflows, Prompt Library helps you keep everything organized and versioned, so you never lose track of your best ideas. 
+        Add new prompts, revisit older versions, and browse your growing prompt collection - all in one simple interface.
         </p>
         <a
           href="/library"
