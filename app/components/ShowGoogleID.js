5520ca2e019d3e76cba81744edb23896278ecb04
Sunil Patel <126328736+spatelsuy@users.noreply.github.com>
2025-11-30 06:46:10 -0500

    Add files via upload

---
+++ app/components/ShowGoogleID.js
@@ -0,0 +1,15 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+
+export default function ShowGoogleID() {
+  const { data: session } = useSession();
+
+  if (!session) return <p>Not logged in</p>;
+
+  return (
+    <div>
+      <p className="text-gray-900">Welcome {session.user.name}</p>
+    </div>
+  );
+}
